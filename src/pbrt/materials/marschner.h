5ca19f8a6d0bfb45d0257c018d1c9a1588bb0d71
Jeffrey Lemein <jeffrey.lemein@gmail.com>
2019-02-13 00:18:32 +0100

    Added input arguments for Marschner shader

--- src/pbrt/materials/marschner.h
+++ src/pbrt/materials/marschner.h
@@ -20,26 +20,43 @@ namespace pbrt {
 // PlasticMaterial Declarations
 class MarschnerMaterial : public Material {
   public:
-    // PlasticMaterial Public Methods
-    MarschnerMaterial(const std::shared_ptr<Texture<Spectrum>> &Kd,
-                    const std::shared_ptr<Texture<Spectrum>> &Ks,
-                    const std::shared_ptr<Texture<Float>> &roughness,
-                    const std::shared_ptr<Texture<Float>> &bumpMap,
-                    bool remapRoughness)
-        : Kd(Kd),
-          Ks(Ks),
-          roughness(roughness),
-          bumpMap(bumpMap),
-          remapRoughness(remapRoughness) {}
+    MarschnerMaterial(Float Ar, 
+                    Float Br, 
+                    Float hairRadius,
+                    Float eta, 
+                    Float eccentricity, 
+                    Float glintScaleFactor, 
+                    Float causticWidth, 
+                    Float causticFade, 
+                    Float causticLimit,
+                    const std::shared_ptr<Texture<Spectrum>> &sigmaA, 
+                    const std::shared_ptr<Texture<Spectrum>> &Kd) 
+        : mAr(Ar), mAtt(-.5 * Ar), mAtrt(-1.5*Ar),                
+        mBr(Br), mBtt(0.5*Br), mBtrt(2.0*Br),
+        mHairRadius(hairRadius), 
+        mEta(eta), 
+        mEccentricity(eccentricity), 
+        mGlintScaleFactor(glintScaleFactor), 
+        mCausticWidth(causticWidth), 
+        mCausticFade(causticFade), 
+        mCausticLimit(causticLimit),
+        mSigmaA(sigmaA), 
+        mKd(Kd) {}
+        
     void ComputeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena,
                                     TransportMode mode,
                                     bool allowMultipleLobes) const;
 
   private:
-    // DualscatteringMaterial Private Data
-    std::shared_ptr<Texture<Spectrum>> Kd, Ks;
-    std::shared_ptr<Texture<Float>> roughness, bumpMap;
-    const bool remapRoughness;
+//    std::shared_ptr<Texture<Spectrum>> Kd, Ks;
+//    std::shared_ptr<Texture<Float>> roughness, bumpMap;
+//    const bool remapRoughness;
+      
+    // Input parameters to Marschner hair scattering
+    Float mAr, mAtt, mAtrt;
+    Float mBr, mBtt, mBtrt;
+    Float mHairRadius, mEta, mEccentricity, mGlintScaleFactor, mCausticWidth, mCausticFade, mCausticLimit;
+    std::shared_ptr<Texture<Spectrum>> mSigmaA, mKd;
 };
 
 MarschnerMaterial *CreateMarschnerMaterial(const TextureParams &mp);
