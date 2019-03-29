0eb79190fedbd3411a7b42c7790a0343ae06f2f4
Jeffrey Lemein <jeffrey.lemein@gmail.com>
2019-03-29 08:06:58 +0100

    Added some rendering results for working marschner (single scattering)

--- experiment/2019_marschner_complete/prepare_all.sh
+++ experiment/2019_marschner_complete/prepare_all.sh
@@ -1,6 +1,6 @@
 rm output/*.pbrt
 
-#prepare --input woman-head.scene --output output/brunette_distantlight_around_x.pbrt --propertyfiles base.properties lighting_around_x.properties --properties image_output_basename=brunette_distantlight_around_x,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
+prepare --input woman-head.scene --output output/brunette_distantlight_around_x.pbrt --propertyfiles base.properties lighting_around_x.properties --properties image_output_basename=brunette_distantlight_around_x,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
 
 #showing caustic width
 #prepare --input woman-head.scene --output output/brunette_distantlight_causticwidth.pbrt --propertyfiles base.properties causticwidth.properties --properties image_output_basename=brunette_glints,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#
@@ -15,13 +15,13 @@ rm output/*.pbrt
 # BRUNETTE MODEL (STRAIGHT HAIR)
 # ---------------------------------------------------
 # distant light
-#prepare --input woman-head.scene --output output/brunette_distantlight_around_y.pbrt --propertyfiles base.properties lighting_around_y.48.properties --properties image_output_basename=brunette_distantlight_around_y,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=
-#prepare --input woman-head.scene --output output/brunette_distantlight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.properties --properties image_output_basename=brunette_distantlight_around_y_elevated,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
-#prepare --input woman-head.scene --output output/brunette_distantlight_around_x.pbrt --propertyfiles base.properties lighting_around_x.48.properties --properties image_output_basename=brunette_distantlight_around_x,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
+#prepare --input woman-head.scene --output output/brunette_distantlight_around_y.pbrt --propertyfiles base.properties lighting_around_y.properties --properties image_output_basename=brunette_distantlight_around_y,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
+#prepare --input woman-head.scene --output output/brunette_distantlight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.properties --properties image_output_basename=brunette_distantlight_around_y_elevated,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=# 
+#prepare --input woman-head.scene --output output/brunette_distantlight_around_x.pbrt --propertyfiles base.properties lighting_around_x.properties --properties image_output_basename=brunette_distantlight_around_x,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=#
 
 # area light
 #prepare --input woman-head.scene --output output/brunette_arealight_around_y.pbrt --propertyfiles base.properties lighting_around_y.properties --properties image_output_basename=brunette_arealight_around_y,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=#,useAreaLight=,useInfiniteLight=#
-#prepare --input woman-head.scene --output output/brunette_arealight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.properties --properties image_output_basename=brunette_arealight_around_y_elevated,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=#,useAreaLight=,useInfiniteLight=#
+#prepare --input woman-head.scene --output output/brunette_arealight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.properties --properties image_output_basename=brunette_arealight_around_y_elevated,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=#,useAreaLight=,useInfiniteLight=
 #prepare --input woman-head.scene --output output/brunette_arealight_around_x.pbrt --propertyfiles base.properties lighting_around_x.properties --properties image_output_basename=brunette_arealight_around_x,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=#,useAreaLight=,useInfiniteLight=#
 
 #
@@ -33,6 +33,7 @@ prepare --input woman-head.scene --output output/brunette_move_around_y.pbrt --p
 # rotating model
 #
 prepare --input woman-head.scene --output output/brunette_rotate_around_y.pbrt --propertyfiles base.properties rotating_model_around_y.properties --properties image_output_basename=brunette_rotating_model_around_y,material_name=brown_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=
+prepare --input woman-head.scene --output output/brunette_rotate_around_y.pbrt --propertyfiles base.properties rotating_model_around_y.properties --properties image_output_basename=brunette_rotating_model_around_y,material_name=blonde_hair,model_hair_filename=wStraight,useDistantLight=,useAreaLight=#,useInfiniteLight=
 
 # ---------------------------------------------------
 # BLONDE MODEL (WAVY HAIR)
@@ -44,7 +45,7 @@ prepare --input woman-head.scene --output output/brunette_rotate_around_y.pbrt -
 
 # area light
 #prepare --input woman-head.scene --output output/blonde_arealight_around_y.pbrt --propertyfiles base.properties lighting_around_y.properties --properties image_output_basename=blonde_arealight_around_y,material_name=blonde_hair,model_hair_filename=wWavy,useDistantLight=#,useAreaLight=,useInfiniteLight=#
-#prepare --input woman-head.scene --output output/blonde_arealight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.48.properties --properties image_output_basename=blonde_arealight_around_y_elevated,material_name=blonde_hair,model_hair_filename=wWavy,useDistantLight=#,useAreaLight=,useInfiniteLight=
+#prepare --input woman-head.scene --output output/blonde_arealight_around_y_elevated.pbrt --propertyfiles base.properties lighting_around_y_elevated.properties --properties image_output_basename=blonde_arealight_around_y_elevated,material_name=blonde_hair,model_hair_filename=wWavy,useDistantLight=#,useAreaLight=,useInfiniteLight=
 #prepare --input woman-head.scene --output output/blonde_arealight_around_x.pbrt --propertyfiles base.properties lighting_around_x.properties --properties image_output_basename=blonde_arealight_around_x,material_name=blonde_hair,model_hair_filename=wWavy,useDistantLight=#,useAreaLight=,useInfiniteLight=#
 
 #
